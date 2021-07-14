9c6a8a45f73ad1c91f13e4eb7feb87cbbd4c20d7
cecep31 <cecepjanuardi31@gmail.com>
2021-07-14 14:24:29 +0700

    success add post

---
+++ models/post.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type Post struct {
+	Id        uint64    `json:"id" gorm:"primary_key"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	Createdat time.Time `json:"createdat"`
+}
