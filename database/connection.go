ca74e26ec7a098493e4674d3332e37887c12d85d
cecep31 <cecepjanuardi31@gmail.com>
2021-07-16 15:02:54 +0700

    ook

--- database/connection.go
+++ database/connection.go
@@ -1,11 +1,14 @@
 package database
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Con() *gorm.DB {
+	dbname := os.Getenv("DB_NAME")
 	dsn := "pilput:pilput31@tcp(127.0.0.1:3306)/learnfiber?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
